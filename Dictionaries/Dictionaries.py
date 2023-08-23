d65e5aff6ef4ca072e846af5eb9d0c2f9bb14ed0
Gathoni <janetgathoni910@gmail.com>
2023-08-23 14:20:55 +0300

    Loop Dictionaries

--- Dictionaries/Dictionaries.py
+++ Dictionaries/Dictionaries.py
@@ -71,5 +71,57 @@ print(dict_items)
 
 if "Brand" in testDict:
     print("We have the car brand now!")
+    
+    
+# Change Dictionary Items
 
+
+# Change Values
+# You can change values by referring to its key name..
+
+testDict["Brand"] = "Range Rover"
+print(testDict)
+
+# Update Dictionary
+# The update() method will update the dictionary with the items from the given argument.
+
+testDict.update({"School" : "Moringa"})
+print(testDict)
+
+# Adding Items
+# Adding an item to the dictionary is done by using a new index key and assigning a value to it:
+
+testDict["Colour"] = "Wine Red"
+print(testDict)
+
+# Remove Dictionary Items
+# Clear () method clears the whole dictionary.
+
+# pop() method 
+# It removes the item with the specified key name:
+# Works same as Del method
+
+testDict.pop("Colour")
+print(testDict)
+
+# popitem()
+# It removes the last inserted item
+
+testDict.popitem()
+print(testDict)
+
+
+# Loop Dictionaries
+
+
+# You can loop through a dictionary by using a for loop.
+# This returns the keys instead of the values
+
+for X in testDict:
+    print(X)
+
+# To get the values you do the following
+for X in testDict.values():
+    print(X)
+    
 
